e5c49bf02baf584ee8c272307ab045fab024940e
gaura <gaurab.dhimal35@gmail.com>
2024-10-09 13:47:16 -0400

    Initial commit; T1-T4 done

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+//Task 1
+        for(int c =0; c <= 30; c++)
+            System.out.print("\t" + c);
+        System.out.println();
+//Task 2
+        for(int c =30; c >= 0; c-=1)
+            System.out.print("\t" + c);
+        System.out.println();
+//Task3
+        for(int c =0; c <= 18; c+=3)
+            System.out.print("\t" + c);
+        System.out.println();
+//Task4
+        for(int c =10; c >= 0; c-=2)
+            System.out.print("\t" + c);
+
+    }
+}
