8a1cb4d5202d8d03c0f87ba9ec5b9bb94f65d64c
gaura <gaurab.dhimal35@gmail.com>
2024-10-10 23:27:14 -0400

    Part B/Lab07 finished

--- src/PartB.java
+++ src/PartB.java
@@ -2,12 +2,37 @@ public class PartB
 {
     public static void main(String[] args)
     {
+        //Task 5
 
+        for (int row = 1; row <= 5; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
+        //Task 6
 
+        for (int row = 5; row >= 1; row--)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
+        //Task 7
 
-
-
+        for (int row = 1; row <= 5; row++)
+        {
+            for (int col = 1; col <= 5; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
